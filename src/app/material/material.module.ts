81c9e241e72812a1b8ecccae515770ba43caab46
Miguel Angel Pérez Contreras <mperez@meraki-s.com>
2021-02-15 17:08:20 -0600

    :package: Add LayoutModule in material module

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { A11yModule } from '@angular/cdk/a11y';
+import { LayoutModule } from '@angular/cdk/layout';
 import { ClipboardModule } from '@angular/cdk/clipboard';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { PortalModule } from '@angular/cdk/portal';
@@ -46,6 +47,7 @@ import { OverlayModule } from '@angular/cdk/overlay';
 @NgModule({
   exports: [
     A11yModule,
+    LayoutModule,
     ClipboardModule,
     CdkStepperModule,
     CdkTableModule,
