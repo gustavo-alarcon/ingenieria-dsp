ff9ecc8d2edf6f99f8d0abadf44b8890a81cb438
Miguel Angel Pérez Contreras <mperez@meraki-s.com>
2021-02-18 09:53:20 -0600

    add function create user

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,14 @@
 const functions = require("firebase-functions");
-
+const admin = require('firebase-admin');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
+admin.initializeApp();
+
 exports.googleChatBot = functions.https.onRequest((request, response) => {
     var HEADER = {
         "title": "Meraki Solutions"
-      };
+    };
 
     const body = 'Prueba desde firestore';
 
@@ -26,3 +28,20 @@ exports.googleChatBot = functions.https.onRequest((request, response) => {
     }
     )
 });
+
+
+exports.createUserSetClaims = functions.auth.user().onCreate(async (user) => {
+    admin.firestore().settings({ timestampsInSnapshots: true });
+    functions.logger.info(`create user ${user.email} ${user.uid}`, { structuredData: true });
+    const role = 'Administrador';
+    await admin.auth().setCustomUserClaims(user.uid, { role });
+    await admin.firestore().collection('users').doc(user.uid).set({
+        email: user.email,
+        uid: user.uid,
+        name: user.displayName,
+        picture: user.photoURL,
+        role
+    }, { merge: true });
+    return;
+});
+
