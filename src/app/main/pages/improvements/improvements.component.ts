18e293b6f770c964bf79f95efc9720fe8d3660a8
Miguel Angel Pérez Contreras <mperez@meraki-s.com>
2021-02-19 19:08:55 -0600

    :bug: fix code

--- src/app/main/pages/improvements/improvements.component.ts
+++ src/app/main/pages/improvements/improvements.component.ts
@@ -2,11 +2,9 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Improvements } from 'src/app/auth/models/inprovenents.model';
-import * as XLSX from 'xlsx';
 import { ImprovementsService } from '../../../auth/services/improvements.service';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { CreateDialogImprovenmentsComponent } from './dialogs/create-dialog-improvenments/create-dialog-improvenments.component';
 import { EditDialogImprovenmentsComponent } from './dialogs/edit-dialog-improvenments/edit-dialog-improvenments.component';
@@ -19,9 +17,6 @@ import { ValidateDialogImprovenmentsComponent } from './dialogs/validate-dialog-
   styleUrls: ['./improvements.component.scss']
 })
 export class ImprovementsComponent implements OnInit {
-
-
-  improvenmentsForm: FormGroup;
 
   dataSource = new MatTableDataSource();
   selected: any;
@@ -42,80 +37,7 @@ export class ImprovementsComponent implements OnInit {
     private fb: FormBuilder,
   ) { }
 
-  ngOnInit(): void {
-
-    this.improvement$ = this.impvServices.getAllImprovement().pipe(
-      tap(res => {
-        if (res) {
-          this.improvementDataSource.data = res;
-        }
-      })
-    );
-  }
-
-  onFileSelectedImprovements(event): void {
-
-    if (event.target.files && event.target.files[0]) {
-
-      const reader = new FileReader();
-      reader.onload = (e: any) => {
-        /* read workbook */
-        const bstr: string = e.target.result;
-        const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
-
-        /* grab first sheet */
-        const wsname: string = wb.SheetNames[0];
-        const ws: XLSX.WorkSheet = wb.Sheets[wsname];
-
-        /* save data */
-        this.selected = XLSX.utils.sheet_to_json(ws, { header: 'A', defval: '' });
-        this.upLoadXlsImprovements(this.selected);
-      };
-      reader.readAsBinaryString(event.target.files[0]);
-    }
-  }
-
-
-  upLoadXlsImprovements(array): void {
-
-    let aux: boolean;
-    array.forEach(el => { // slice(1)
-      console.log('el : ', el);
-      aux = false;
-      const array2: Array<string> = Object.values(el);
-      /*
-      aux = false;
-      let array2: Array<string> = Object.values(el);
-      //probando si nombre ya existe. si es asi, ejecuta editTagDestinations.
-      //sino, crea nuevo destino.
-       this.dataSourceListaDestinos.data.forEach(element =>{
-        if(element["name"].toLowerCase() === array2[0].toLowerCase()){
-          this.dbs.editTagDestinations({
-            name: array2[0],
-            description: array2[1],
-            id: element["id"]
-          });
-          aux=true;
-        }
-      });
-      console.log('array 2 : ',array2);
-      console.log('aux : ',aux);
-      */
-
-      aux === false ? this.impvServices.addTagDestinations({
-        date: array2[0],
-        name: array2[1],
-        component: array2[2],
-        model: array2[3],
-        review: array2[4],
-        user: array2[5],
-        state: array2[6],
-        // tslint:disable-next-line: no-unused-expression
-      }) : null;
-    });
-    // Se limpia entrada de documento en el HTML
-    document.getElementById('fileInput2').nodeValue = '';
-  }
+  ngOnInit(): void { }
 
 
   openDialog(value: string, index?: number): void {
