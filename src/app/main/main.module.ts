a8e3b240f7827a015acb69cfe752c44641338a45
Miguel Angel Pérez Contreras <mperez@meraki-s.com>
2021-02-20 10:35:53 -0600

    :art: Dialog delete

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -5,15 +5,12 @@ import { MaterialModule } from '../material/material.module';
 import { MainRoutingModule } from './main-routing.module';
 
 import { MainComponent } from './main/main.component';
-import { DialogValidationLogisticsComponent } from './components/dialog-validation-logistics/dialog-validation-logistics.component';
-import { DialogInsertImprovementsComponent } from './components/dialog-insert-improvements/dialog-insert-improvements.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     MainComponent,
-    DialogValidationLogisticsComponent,
-    DialogInsertImprovementsComponent
+
   ],
   exports: [
     MainComponent
