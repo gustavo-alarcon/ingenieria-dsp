44c7c5e73877b09441ba03b25ac4ce2b1eca3cfd
Gustavo Alarcon <galarcon@meraki-s.com>
2021-02-22 08:26:58 -0500

    :art: Spare parts

--- src/app/main/pages/spare-parts/spare-parts.component.ts
+++ src/app/main/pages/spare-parts/spare-parts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import * as XLSX from 'xlsx';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ImprovementsService } from '../../services/improvements.service';
@@ -9,25 +9,25 @@ import { ImprovementsService } from '../../services/improvements.service';
   styleUrls: ['./spare-parts.component.scss']
 })
 export class SparePartsComponent implements OnInit {
-  
+
+  @ViewChild("fileInput2", {read: ElementRef}) fileButton: ElementRef;
+
   checked: boolean = false;
-  selectCkeck:number;
-  selected:any;
+  selectCkeck: number;
+  selected: any;
 
-  dataSparePart:SparePart[]=[];
+  dataSparePart: SparePart[] = [];
 
 
-  constructor( 
-              private snackBar: MatSnackBar,
-              private impvServices:ImprovementsService,
-    ) { }
+  constructor(
+    private snackBar: MatSnackBar,
+    private impServices: ImprovementsService,
+  ) { }
 
   ngOnInit(): void {
   }
 
   onFileSelected(event): void {
-    console.log('event : ',event);
-
     if (event.target.files && event.target.files[0]) {
       let name = event.target.files[0].name
 
@@ -40,83 +40,78 @@ export class SparePartsComponent implements OnInit {
         const wsname: string = wb.SheetNames[0];
         const ws: XLSX.WorkSheet = wb.Sheets[wsname];
         /* save data */
-        this.selected = XLSX.utils.sheet_to_json(ws, { header: "A", defval: "" });
-        this.upLoadXls(this.selected, name.split(".")[0])
+        this.selected = XLSX.utils.sheet_to_json(ws, { header: 1 });
+        this.upLoadXls(this.selected)
       };
       reader.readAsBinaryString(event.target.files[0]);
     }
   }
-  
-  changeValue(value,i) {
+
+  changeValue(value, i) {
     console.log(value)
     console.log(i)
 
     this.checked = !value;
-    this.selectCkeck=i;
+    this.selectCkeck = i;
   }
 
-  upLoadXls(array, name): void {  
-    console.log('array : ',array)
-
+  upLoadXls(array): void {
     let arraydData = [];
-    if (array.length>0) {
 
-      array.shift();
-      console.log('array : ',array)
-          
-
-       array.forEach(el => {
+    if (array.length > 0) {
+      array.forEach(el => {
         let array2 = Object.values(el);
-        //let array2: Array<string> = Object.values(el);          
-          let data = 
-            {
-              partN:array2[0], 
-              qty: array2[1],
-              type:array2[2],
-              smcs:array2[3],
-              partName:array2[4],
-              groupN:array2[5],
-              groupName:array2[6],
-              note:array2[7],
-              newGroupN:this.impvServices.getCurrent_Improv(array2[5]),
-             
-            }
-            arraydData.push(data)        
+        //let array2: Array<string> = Object.values(el);        
+        let data =
+        {
+          partN: array2[0],
+          qty: array2[1],
+          type: array2[2],
+          smcs: array2[3],
+          partName: array2[4],
+          groupN: array2[5],
+          groupName: array2[6],
+          note: array2[7],
+          newGroupN: this.impServices.getCurrent_Improv(array2[5]),
+
+        }
+        arraydData.push(data)
       });
-      console.log('arraydData : ',arraydData);
-      
-      this.dataSparePart=arraydData;
 
-    }else{
+      console.log('arraydData : ', arraydData);
+
+      this.dataSparePart = arraydData;
+
+      this.fileButton.nativeElement.value = null;
+    } else {
       this.snackBar.open("el archivo esta vacio ", "Aceptar", {
-        duration: 3000,});
+        duration: 3000,
+      });
     }
-    
-   /*   this.currentData=arraydData;
 
-    this.settingDataSource.data = this.currentData; */
-  console.log('this.dataSpare : ', this.dataSparePart);
-  
-   document.getElementById("fileInput2").nodeValue = "";
+    /*   this.currentData=arraydData;
+ 
+     this.settingDataSource.data = this.currentData; */
+    console.log('this.dataSpare : ', this.dataSparePart);
   }
 
-  deleteControl(){
+  deleteControl() {
 
   }
-  downloadData(){
-    
+  downloadData() {
+
   }
 
 }
 
-export interface SparePart{
-  partN     :string;
-  qty       :number;
-  type      :string;
-  smcs      :number;
-  partName  :string;
-  groupN    :string;
-  groupName :string;
-  note      :string;
-  newGroupN :string;
+export interface SparePart {
+  partN: string;
+  qty: number;
+  type: string;
+  smcs: number;
+  partName: string;
+  groupN: string;
+  groupName: string;
+  note: string;
+  newGroupN: string;
 }
