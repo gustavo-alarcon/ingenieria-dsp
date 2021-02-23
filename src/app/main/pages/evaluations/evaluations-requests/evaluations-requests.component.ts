30651ab6bffe5aa33fe23129cd9b741061246d8f
Marcel <marcelldeveloper@gmail.com>
2021-02-23 10:05:40 -0500

    add request

---
+++ src/app/main/pages/evaluations/evaluations-requests/evaluations-requests.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-evaluations-requests',
+  templateUrl: './evaluations-requests.component.html',
+  styleUrls: ['./evaluations-requests.component.scss']
+})
+export class EvaluationsRequestsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/main/pages/evaluations/evaluations.module.ts
+++ src/app/main/pages/evaluations/evaluations.module.ts
@@ -3,13 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { EvaluationsRoutingModule } from './evaluations-routing.module';
 import { EvaluationsComponent } from './evaluations.component';
+import { MaterialModule } from 'src/app/material/material.module';
+import { EvaluationsRequestsComponent } from './evaluations-requests/evaluations-requests.component';
+import { EvaluationsProcessComponent } from './evaluations-process/evaluations-process.component';
 
 
 @NgModule({
-  declarations: [EvaluationsComponent],
+  declarations: [EvaluationsComponent, EvaluationsRequestsComponent, EvaluationsProcessComponent],
   imports: [
     CommonModule,
-    EvaluationsRoutingModule
+    EvaluationsRoutingModule,
+    MaterialModule,
+
   ]
 })
 export class EvaluationsModule { }
