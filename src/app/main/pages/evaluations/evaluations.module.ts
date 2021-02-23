85fb318546a9c78b1c751fad192ec8156d5c832c
Marcel <marcelldeveloper@gmail.com>
2021-02-23 17:35:35 -0500

    🚧 in tab request

--- src/app/main/pages/evaluations/evaluations.module.ts
+++ src/app/main/pages/evaluations/evaluations.module.ts
@@ -8,18 +8,28 @@ import { EvaluationsRequestsComponent } from './evaluations-requests/evaluations
 import { EvaluationsProcessComponent } from './evaluations-process/evaluations-process.component';
 import { EvaluationsHistoryComponent } from './evaluations-history/evaluations-history.component';
 import { EvaluationsSettingsComponent } from './evaluations-settings/evaluations-settings.component';
-
+import {RequestsSettingDialogComponent} from './requests-setting-dialog/requests-setting-dialog.component';
+import {RequestsStartDialogComponent} from './requests-start-dialog/requests-start-dialog.component';
+import {RequestsObservationDialogComponent} from './requests-observation-dialog/requests-observation-dialog.component';
+import {RequestsTimeLineDialogComponent} from './requests-time-line-dialog/requests-time-line-dialog.component';
 
 @NgModule({
-  declarations: [EvaluationsComponent, EvaluationsRequestsComponent, EvaluationsProcessComponent, EvaluationsHistoryComponent, EvaluationsSettingsComponent],
-  exports: [
-    EvaluationsComponent
+  declarations: [
+    EvaluationsComponent,
+    EvaluationsRequestsComponent,
+    EvaluationsProcessComponent,
+    EvaluationsHistoryComponent,
+    EvaluationsSettingsComponent,
+    RequestsSettingDialogComponent,
+    RequestsStartDialogComponent,
+    RequestsObservationDialogComponent,
+    RequestsTimeLineDialogComponent
   ],
+  exports: [EvaluationsComponent],
   imports: [
     CommonModule,
     EvaluationsRoutingModule,
     MaterialModule,
-
-  ]
+  ],
 })
-export class EvaluationsModule { }
+export class EvaluationsModule {}
