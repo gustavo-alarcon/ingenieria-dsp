6d0148ff8c83e523171210268deb7013cfdb63ea
Miguel Angel Pérez Contreras <mperez@meraki-s.com>
2021-02-24 13:12:24 -0600

    :art: dialog finalize

--- src/app/main/pages/evaluations/evaluations-progress/dialogs/evaluations-finalize-dialog/evaluations-finalize-dialog.component.ts
+++ src/app/main/pages/evaluations/evaluations-progress/dialogs/evaluations-finalize-dialog/evaluations-finalize-dialog.component.ts
@@ -1,13 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { BehaviorSubject } from 'rxjs';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-evaluations-finalize-dialog',
   templateUrl: './evaluations-finalize-dialog.component.html',
-  styleUrls: ['./evaluations-finalize-dialog.component.sass']
+  styleUrls: ['./evaluations-finalize-dialog.component.scss']
 })
 export class EvaluationsFinalizeDialogComponent implements OnInit {
 
-  constructor() { }
+
+  loading = new BehaviorSubject<boolean>(false);
+  loading$ = this.loading.asObservable();
+
+  constructor(
+        @Inject(MAT_DIALOG_DATA) public data: any,
+        public dialogRef: MatDialogRef<EvaluationsFinalizeDialogComponent>,
+
+  ) { }
 
   ngOnInit(): void {
   }
--- src/app/main/pages/evaluations/evaluations-progress/evaluations-progress.module.ts
+++ src/app/main/pages/evaluations/evaluations-progress/evaluations-progress.module.ts
@@ -9,6 +9,7 @@ import { EvaluationsSettingsDialogComponent } from './dialogs/evaluations-settin
 import { EvaluationsFinalizeDialogComponent } from './dialogs/evaluations-finalize-dialog/evaluations-finalize-dialog.component';
 import { EvaluationsImagesDialogComponent } from './dialogs/evaluations-images-dialog/evaluations-images-dialog.component';
 import { EvaluationsTimeLineDialogComponent } from './dialogs/evaluations-time-line-dialog/evaluations-time-line-dialog.component';
+// tslint:disable-next-line: max-line-length
 import { EvaluationsObservationsDialogComponent } from './dialogs/evaluations-observations-dialog/evaluations-observations-dialog.component';
 import { EvaluationsConsultsDialogComponent } from './dialogs/evaluations-consults-dialog/evaluations-consults-dialog.component';
 
