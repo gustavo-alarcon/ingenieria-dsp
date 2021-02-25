6a3c2f2ca0d29b6a2cb5a024bf023a9d778299cf
Miguel Angel Pérez Contreras <mperez@meraki-s.com>
2021-02-24 21:49:15 -0600

    :art: Add observation dialog no image & questions

--- src/app/main/pages/evaluations/evaluations-progress/dialogs/evaluations-observations-dialog/evaluations-observations-dialog.component.ts
+++ src/app/main/pages/evaluations/evaluations-progress/dialogs/evaluations-observations-dialog/evaluations-observations-dialog.component.ts
@@ -1,13 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { BehaviorSubject } from 'rxjs';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-evaluations-observations-dialog',
   templateUrl: './evaluations-observations-dialog.component.html',
-  styleUrls: ['./evaluations-observations-dialog.component.sass']
+  styleUrls: ['./evaluations-observations-dialog.component.scss']
 })
 export class EvaluationsObservationsDialogComponent implements OnInit {
 
-  constructor() { }
+
+  loading = new BehaviorSubject<boolean>(false);
+  loading$ = this.loading.asObservable();
+
+  constructor(
+        @Inject(MAT_DIALOG_DATA) public data: any,
+        public dialogRef: MatDialogRef<EvaluationsObservationsDialogComponent>,
+
+  ) { }
 
   ngOnInit(): void {
   }
