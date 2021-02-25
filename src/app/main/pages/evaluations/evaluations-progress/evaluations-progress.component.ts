0f93a2d0302a4349f6489c381c8cd502b86040d0
Miguel Angel Pérez Contreras <mperez@meraki-s.com>
2021-02-24 20:22:06 -0600

    :art: add observation evaluation click

--- src/app/main/pages/evaluations/evaluations-progress/evaluations-progress.component.ts
+++ src/app/main/pages/evaluations/evaluations-progress/evaluations-progress.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+
 import { EvaluationsConsultsDialogComponent } from './dialogs/evaluations-consults-dialog/evaluations-consults-dialog.component';
 import { EvaluationsFinalizeDialogComponent } from './dialogs/evaluations-finalize-dialog/evaluations-finalize-dialog.component';
 import { EvaluationsImagesDialogComponent } from './dialogs/evaluations-images-dialog/evaluations-images-dialog.component';
+// tslint:disable-next-line: max-line-length
+import { EvaluationsObservationsDialogComponent } from './dialogs/evaluations-observations-dialog/evaluations-observations-dialog.component';
 import { EvaluationsResponseDialogComponent } from './dialogs/evaluations-response-dialog/evaluations-response-dialog.component';
 import { EvaluationsSettingsDialogComponent } from './dialogs/evaluations-settings-dialog/evaluations-settings-dialog.component';
 
@@ -47,22 +50,18 @@ export class EvaluationsProgressComponent implements OnInit {
     });
   }
 
-  responseDialog():void{
+  responseDialog(): void {
     this.dialog.open(EvaluationsResponseDialogComponent, {
       width: '35%',
       height: '90%'
     });
   }
 
-  // obsDialog(): void {
-  //   this.dialog.open(RequestsObservationDialogComponent, {
-  //     width: '35%',
-  //   });
-  // }
-  // timeline(): void {
-  //   this.dialog.open(RequestsTimeLineDialogComponent, {
-  //     width: '90%',
-  //   });
-  // }
+  observationsDialog(): void {
+    this.dialog.open(EvaluationsObservationsDialogComponent, {
+      width: '35%',
+      height: '90%'
+    });
+  }
 
 }
