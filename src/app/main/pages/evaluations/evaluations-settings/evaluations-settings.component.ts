fa33e579013b94c8c401d7524773bb8f14026fa7
Miguel Angel Pérez Contreras <mperez@meraki-s.com>
2021-03-02 10:59:31 -0600

    :art: Add expansions panels no styles

--- src/app/main/pages/evaluations/evaluations-settings/evaluations-settings.component.ts
+++ src/app/main/pages/evaluations/evaluations-settings/evaluations-settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-evaluations-settings',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EvaluationsSettingsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public auth: AuthService
+  ) { }
 
   ngOnInit(): void {
   }
