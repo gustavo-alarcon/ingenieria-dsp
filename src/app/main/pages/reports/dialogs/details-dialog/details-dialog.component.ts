e64ff32f9a4b6606203eb2905a1b89fc8e72aede
Marcel <marcelldeveloper@gmail.com>
2021-03-03 11:42:14 -0500

    🎨 add delete, details, return in dialog reports

---
+++ src/app/main/pages/reports/dialogs/details-dialog/details-dialog.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-details-dialog',
+  templateUrl: './details-dialog.component.html',
+  styleUrls: ['./details-dialog.component.scss']
+})
+export class DetailsDialogComponent implements OnInit {
+  
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+
+}
--- src/app/main/pages/reports/reports.component.ts
+++ src/app/main/pages/reports/reports.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { DeleteDialogComponent } from './dialogs/delete-dialog/delete-dialog.component';
+import { DetailsDialogComponent } from './dialogs/details-dialog/details-dialog.component';
+import { ReturnDialogComponent } from './dialogs/return-dialog/return-dialog.component';
 
 @Component({
   selector: 'app-reports',
@@ -9,16 +13,35 @@ import { FormGroup } from '@angular/forms';
 export class ReportsComponent implements OnInit {
   searchForm: FormGroup;
 
-  constructor() { }
+  constructor(    public dialog: MatDialog,
+    ) { }
 
   ngOnInit(): void {
   }
   editDialog(): void{
 
   }
+  returnDialog(): void{
+    this.dialog.open(ReturnDialogComponent, {
+      maxWidth: 500,
+      width: '90vw',
+      //data: item,
+    });
+  }
+  detailsDialog(): void{
+    this.dialog.open(DetailsDialogComponent, {
+      maxWidth: 500,
+      width: '60vw',
+      //data: item,
+    });
+  }
 
   deleteDialog(): void{
-
-  }
+      this.dialog.open(DeleteDialogComponent, {
+        maxWidth: 500,
+        width: '90vw',
+        //data: item,
+      });
+    }
 
 }
