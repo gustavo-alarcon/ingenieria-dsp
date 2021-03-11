3bc631cf99215ac5cf7ea7f25399af1169174679
Marcel <marcelldeveloper@gmail.com>
2021-03-11 18:18:16 -0500

    📦 responsive in records

--- src/app/main/pages/andon/record/record.component.ts
+++ src/app/main/pages/andon/record/record.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { Observable, combineLatest } from 'rxjs';
@@ -9,6 +9,7 @@ import { tap, debounceTime, filter, startWith, map } from 'rxjs/operators';
 import { MatDialog } from '@angular/material/dialog';
 import { ImageDialogComponent } from './dialog/image-dialog/image-dialog.component';
 import * as XLSX from 'xlsx';
+import { EvaluationsService } from '../../../services/evaluations.service';
 
 @Component({
   selector: 'app-record',
@@ -18,7 +19,8 @@ import * as XLSX from 'xlsx';
 export class RecordComponent implements OnInit {
   searchForm: FormGroup;
   records$: Observable<Andon[]>;
-
+  dateForm: FormGroup;
+  
   historyDataSource = new MatTableDataSource<Andon>();
   historyDisplayedColumns: string[] = [
     'reportDate',
@@ -41,8 +43,26 @@ export class RecordComponent implements OnInit {
   ) {
     this.historyDataSource.paginator = paginator;
   }
+  historyMobilDataSource = new MatTableDataSource<Andon>();
+  historyMobilDisplayedColumns: string[] = [
+    'state',
+    'reportDate',
+    'workShop',
+    'name',
+    'otChild',
+    'problemType',
+    'description',
+    'atentionTime',
+    'actions',
+  ];
 
+  @ViewChild('historyMobilPaginator', { static: false }) set content1(
+    paginator: MatPaginator
+  ) {
+    this.historyMobilDataSource.paginator = paginator;
+  }
   constructor(
+     private dbs: EvaluationsService,
      private fb: FormBuilder,
      private andonService: AndonService,
      public dialog: MatDialog,
@@ -52,6 +72,16 @@ export class RecordComponent implements OnInit {
     this.searchForm = this.fb.group({
       search: ['', Validators.required],
     });
+    const view = this.dbs.getCurrentMonthOfViewDate();
+
+    const beginDate = view.from;
+    const endDate = new Date();
+    endDate.setHours(23, 59, 59);
+
+    this.dateForm = new FormGroup({
+      start: new FormControl(beginDate),
+      end: new FormControl(endDate),
+    });
 
     this.records$ = combineLatest(
       this.andonService.getAllAndon(),
@@ -59,24 +89,41 @@ export class RecordComponent implements OnInit {
         debounceTime(300),
         filter((input) => input !== null),
         startWith<any>('')
+      ),
+      this.dateForm.get('start').valueChanges.pipe(
+        startWith(beginDate),
+        map(begin => begin.setHours(0, 0, 0, 0))
+      ),
+      this.dateForm.get('end').valueChanges.pipe(
+        startWith(endDate),
+        map(end =>  end ? end.setHours(23, 59, 59) : null)
       )
     ).pipe(
-      map(([andons, search]) => {
+      map(([andons, search, startdate, enddate]) => {
+
+        const date = {begin: startdate, end: enddate};
+
         const searchTerm = search.toLowerCase().trim();
         let preFilterSearch: Andon[] = [...andons];
 
-        preFilterSearch = andons.filter((andon) => {
-          return (
-            String(andon.name).toLowerCase().includes(searchTerm) ||
-            String(andon.otChild).toLowerCase().includes(searchTerm)
-          );
-        });
+       
+
+
         return preFilterSearch;
+
       }),
       tap(res => {
         this.historyDataSource.data = res;
+        this.historyMobilDataSource.data = res;
       })
     );
+  }
+
+  getFilterTime(el, time): any {
+    const date = el.toMillis();
+    const begin = time.begin;
+    const end = time.end;
+    return date >= begin && date <= end;
   }
 
   imageDialog(item): void {
