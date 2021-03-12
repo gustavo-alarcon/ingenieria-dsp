bbf0bfe47edf879b41e5a5f19b14b357dc4416a9
Miguel Angel Pérez Contreras <mperez@meraki-s.com>
2021-03-12 16:44:56 -0600

    :art: image component progress evaluation

--- src/app/shared/components/upload-task/upload-task.component.ts
+++ src/app/shared/components/upload-task/upload-task.component.ts
@@ -1,17 +1,24 @@
-import { Component, OnInit, Input, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnDestroy } from '@angular/core';
 import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/storage';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { finalize, tap } from 'rxjs/operators';
+import { Ng2ImgMaxService } from 'ng2-img-max';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { EvaluationsService } from 'src/app/main/services/evaluations.service';
 
 @Component({
   selector: 'app-upload-task',
   templateUrl: './upload-task.component.html',
   styleUrls: ['./upload-task.component.scss']
 })
-export class UploadTaskComponent implements OnInit {
+export class UploadTaskComponent implements OnInit, OnDestroy {
 
   @Input() file: File;
+  @Input() pathStorage: string;
+
+  // tslint:disable-next-line: no-output-on-prefix
+  @Output() onNewImage: EventEmitter<string> = new EventEmitter<string>();
 
   task: AngularFireUploadTask;
 
@@ -19,39 +26,39 @@ export class UploadTaskComponent implements OnInit {
   snapshot: Observable<any>;
   downloadURL: string;
 
-  constructor(private storage: AngularFireStorage, private db: AngularFirestore) { }
+  subcription = new Subscription();
+  constructor(
+    private storage: AngularFireStorage,
+    private ng2ImgMax: Ng2ImgMaxService,
+  ) { }
 
   ngOnInit(): void {
     this.startUpload();
   }
 
   startUpload(): void {
-
-    // The storage path
-    const path = `test/${Date.now()}_${this.file.name}`;
-
-    // Reference to storage bucket
-    const ref = this.storage.ref(path);
-
-    // The main task
-    this.task = this.storage.upload(path, this.file);
-
-    // Progress monitoring
-    this.percentage = this.task.percentageChanges();
-
-    this.snapshot = this.task.snapshotChanges().pipe(
-      tap(console.log),
-      // The file's download URL
-      finalize(async () => {
-        this.downloadURL = await ref.getDownloadURL().toPromise();
-
-        this.db.collection('files').add({ downloadURL: this.downloadURL, path });
-      }),
+    this.subcription.add(
+      this.ng2ImgMax.resize([this.file], 800, 10000).subscribe((result) => {
+        const path = `${this.pathStorage}-${new Date()}-${result.name}.png`;
+        const ref = this.storage.ref(path);
+        this.task = this.storage.upload(path, result);
+        this.percentage = this.task.percentageChanges();
+        this.snapshot = this.task.snapshotChanges().pipe(
+          finalize(async () => {
+            this.downloadURL = await ref.getDownloadURL().toPromise();
+            this.onNewImage.emit(this.downloadURL);
+          }),
+        );
+      })
     );
   }
 
   isActive(snapshot): boolean {
     return snapshot.state === 'running' && snapshot.bytesTransferred < snapshot.totalBytes;
+  }
+
+  ngOnDestroy(): void {
+    this.subcription.unsubscribe();
   }
 
 }
