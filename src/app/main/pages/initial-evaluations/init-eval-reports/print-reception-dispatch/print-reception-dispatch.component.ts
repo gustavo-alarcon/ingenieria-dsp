78c31c47d91c82a2a4436b8cc63556f8531d85cd
Gustavo Alarcon <galarcon@meraki-s.com>
2021-03-26 15:47:49 -0500

    :bug: Fix month in initial eval report

--- src/app/main/pages/initial-evaluations/init-eval-reports/print-reception-dispatch/print-reception-dispatch.component.ts
+++ src/app/main/pages/initial-evaluations/init-eval-reports/print-reception-dispatch/print-reception-dispatch.component.ts
@@ -117,7 +117,7 @@ export class PrintReceptionDispatchComponent implements OnInit {
       // footer
       const now = new Date();
       const dateString = String(now.getDate()).padStart(2, '0') + '/'
-        + String(now.getMonth()).padStart(2, '0') + '/'
+        + String(now.getMonth()+1).padStart(2, '0') + '/'
         + now.getFullYear() + ' '
         + String(now.getHours()).padStart(2, '0') + ':'
         + String(now.getMinutes()).padStart(2, '0') + ':'
