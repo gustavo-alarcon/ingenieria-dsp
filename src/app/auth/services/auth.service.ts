c3e5b3e3d77ec7d77c0ee55a8749a376bb5493fe
Gustavo Alarcon <galarcon@meraki-s.com>
2021-04-04 09:27:12 -0500

    :rocket: 3.2.5

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -20,7 +20,7 @@ export class AuthService {
 
   user$: Observable<User>;
   version$: Observable<GeneralConfig>;
-  version: string = 'V3.1.5r';
+  version: string = 'V3.2.5r';
 
   constructor(
     private afAuth: AngularFireAuth,
