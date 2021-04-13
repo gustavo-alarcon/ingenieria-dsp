9be76748ccb6e7c4f555881cb079f91783f5cf12
Marcel <marcelldeveloper@gmail.com>
2021-04-13 10:21:40 -0500

    🐛

--- src/app/main/pages/quality/analysis/tracing/tracing.component.ts
+++ src/app/main/pages/quality/analysis/tracing/tracing.component.ts
@@ -1,5 +1,5 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { BehaviorSubject, combineLatest, Observable, Subscription } from 'rxjs';
@@ -204,7 +204,7 @@ export class TracingComponent implements OnInit, OnDestroy {
 
   }
   ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    this.subscriptions.unsubscribe();
   }
 
   settingDialog(): void {
