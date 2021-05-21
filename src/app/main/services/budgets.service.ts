68cc534de16ceafc8ab9ecc099d32901e3e46602
Carlos Dubon <cdubon@meraki-s.com>
2021-05-21 08:27:35 -0600

    :construction: working on service

--- src/app/main/services/budgets.service.ts
+++ src/app/main/services/budgets.service.ts
@@ -24,7 +24,27 @@ export class BudgetsService {
         const budgetsDocRef = this.afs.firestore
           .collection(`/db/ferreyros/budgets`)
           .doc();
+
         list[j].id = budgetsDocRef.id;
+
+        // The for loop runs immediately to completion while all your asynchronous operations are started.
+        // When they complete some time in the future and call their callbacks, the value of your loop index
+        // variable i will be at its last value for all the callbacks.
+
+        // Use .forEach() to iterate since it creates its own function closure
+        // each iteration of the loop can have it's own value
+        ((index: number, list: Array<budgetsExcelColumns>) => {
+          this.afs.firestore
+            .collection(`/db/ferreyros/budgets`)
+            .where('woChild', '==', `${list[index]}`)
+            .get()
+            .then((querySnapshot) => {
+              querySnapshot.forEach((doc) => {
+                console.log(doc.data());
+              });
+            });
+          console.log(j, list);
+        })(j, list);
 
         batch.set(budgetsDocRef, list[j]);
       }
