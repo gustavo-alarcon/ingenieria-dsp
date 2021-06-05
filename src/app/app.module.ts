9dfabbd2e5c126263d0eb89e8ddbf2edb07ab451
Carlos Dubon <cdubon@meraki-s.com>
2021-06-05 09:48:45 -0600

    :construction: working on budgets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -46,10 +46,10 @@ import { USE_EMULATOR as USE_FUNCTIONS_EMULATOR } from '@angular/fire/functions'
   entryComponents: [UpdateReadyComponent],
   providers: [
     { provide: BUCKET, useValue: environment.firebase.storageBucket },
-    // { provide: USE_AUTH_EMULATOR, useValue: environment.useEmulators ? ['localhost', 9099] : undefined },
+    { provide: USE_AUTH_EMULATOR, useValue: environment.useEmulators ? ['localhost', 9099] : undefined },
     { provide: USE_FIRESTORE_EMULATOR, useValue: environment.useEmulators ? ['localhost', 8080] : undefined },
-    { provide: USE_FUNCTIONS_EMULATOR, useValue: environment.useEmulators ? ['localhost', 5001] : undefined }
+    { provide: USE_FUNCTIONS_EMULATOR, useValue: environment.useEmulators ? ['localhost', 5001] : undefined },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
