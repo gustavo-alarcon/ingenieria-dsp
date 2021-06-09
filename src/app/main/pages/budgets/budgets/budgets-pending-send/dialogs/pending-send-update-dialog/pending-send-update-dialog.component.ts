b8cba8c31aae356871e974557437ad3174c508d6
Carlos Dubon <cdubon@meraki-s.com>
2021-06-09 12:07:27 -0600

    :construction: step 2

--- src/app/main/pages/budgets/budgets/budgets-pending-send/dialogs/pending-send-update-dialog/pending-send-update-dialog.component.ts
+++ src/app/main/pages/budgets/budgets/budgets-pending-send/dialogs/pending-send-update-dialog/pending-send-update-dialog.component.ts
@@ -1,5 +1,12 @@
+import { BudgetsService } from './../../../../../../services/budgets.service';
 import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
-import { Component, Inject, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Inject,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import {
   FormArray,
   FormBuilder,
@@ -9,6 +16,16 @@ import {
   Validators,
 } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { BudgetsBroadcastList } from 'src/app/main/models/budgets.model';
+import { map, startWith } from 'rxjs/operators';
+import {
+  MatAutocomplete,
+  MatAutocompleteSelectedEvent,
+} from '@angular/material/autocomplete';
+import { CdkTextareaAutosize } from '@angular/cdk/text-field';
+import { COMMA, ENTER, SPACE, TAB } from '@angular/cdk/keycodes';
+import { MatChipInputEvent } from '@angular/material/chips';
 
 @Component({
   selector: 'app-pending-send-update-dialog',
@@ -25,16 +42,46 @@ export class PendingSendUpdateDialogComponent implements OnInit {
   secondFormGroup: FormGroup;
 
   filesFormGroup: FormGroup;
+  form: FormGroup;
 
   additionalsDropDownOptions = [
     { value: 'parallel', viewValue: 'Paralelo' },
     { value: 'budget', viewValue: 'Presupuesto' },
   ];
 
+  public selectable: boolean = true;
+  public removable: boolean = true;
+  public separatorKeysCodes: number[] = [ENTER, COMMA, SPACE, TAB];
+
+  public emailCtrl: FormControl = new FormControl();
+  public filteredEmails: Observable<string[]>;
+  // List of emails that the budget is going to be sent:
+  public emails: string[] = [];
+
+  // Broadcast lists
+  public broadcastListsNames: string[] = [];
+  public broadcastLists: BudgetsBroadcastList[] = [];
+
+  // Loaders
+  loading: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  loading$: Observable<boolean> = this.loading.asObservable();
+
+  // Arrays containing files the user uploaded
+  public budgetFilesList: Array<File> = [];
+  public reportFilesList: Array<File> = [];
+  public quotationFilesList: Array<File> = [];
+
+  public emailsValidation: boolean = false;
+
+  @ViewChild('emailInput') emailInput: ElementRef<HTMLInputElement>;
+  @ViewChild('auto') matAutocomplete: MatAutocomplete;
+  @ViewChild('autosize') autosize: CdkTextareaAutosize;
+
   constructor(
     public dialogRef: MatDialogRef<PendingSendUpdateDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
-    private _formBuilder: FormBuilder
+    private _formBuilder: FormBuilder,
+    private _budgetService: BudgetsService
   ) {}
 
   ngOnInit(): void {
@@ -61,11 +108,89 @@ export class PendingSendUpdateDialogComponent implements OnInit {
       secondCtrl: ['', Validators.required],
     });
 
-    this.filesFormGroup.value;
+    this.form = this._formBuilder.group({
+      subject: ['', Validators.required],
+      body: ['', Validators.required],
+      observations: '',
+    });
+
+    this._budgetService
+      .getAllBroadcastList()
+      .subscribe((broadcastLists: BudgetsBroadcastList[]) => {
+        broadcastLists.forEach((broadcastList: BudgetsBroadcastList) => {
+          this.broadcastLists.push(broadcastList);
+          this.broadcastListsNames.push(broadcastList.name);
+
+          this.filteredEmails = this.emailCtrl.valueChanges.pipe(
+            startWith(null),
+            map((email: string | null) =>
+              email ? this._filter(email) : this.broadcastListsNames.slice()
+            )
+          );
+        });
+      });
   }
 
   get additionalForms(): FormArray {
     return this.filesFormGroup.get('additionals') as FormArray;
+  }
+
+  get subject() {
+    return this.form.get('subject');
+  }
+
+  get body() {
+    return this.form.get('body');
+  }
+
+  get observations() {
+    return this.form.get('observations');
+  }
+
+  public addEmail(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    // Add our email
+    if (value) {
+      this.emails.push(value);
+    }
+
+    // Reset the input value
+    if (event.input) {
+      event.input.value = '';
+    }
+
+    this.emailCtrl.setValue(null);
+  }
+
+  public removeEmail(email: string): void {
+    const index = this.emails.indexOf(email);
+
+    if (index >= 0) {
+      this.emails.splice(index, 1);
+    }
+  }
+
+  public selectedEmail(event: MatAutocompleteSelectedEvent): void {
+    // Get all the emails from the group
+    const broadcastList: BudgetsBroadcastList = this.broadcastLists.filter(
+      (list) => list.name == event.option.viewValue
+    )[0];
+    broadcastList.emailList.forEach((email: string) => {
+      this.emails.push(email);
+    });
+
+    // check this
+    this.emailInput.nativeElement.value = '';
+    this.emailCtrl.setValue(null);
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.broadcastListsNames.filter(
+      (email) => email.toLowerCase().indexOf(filterValue) === 0
+    );
   }
 
   addAdditional(): void {
@@ -79,6 +204,39 @@ export class PendingSendUpdateDialogComponent implements OnInit {
 
   deleteAdditional(i: number) {
     this.additionalForms.removeAt(i);
+  }
+
+  public loadFilesForBudget(fileList: FileList): void {
+    this.loading.next(true);
+    // Convert FileList to an actual Array
+    const fileListArray: File[] = Array.from(fileList);
+
+    fileListArray.forEach((file: File) => {
+      this.budgetFilesList.push(file);
+    });
+    this.loading.next(false);
+  }
+
+  public loadFilesForReport(fileList: FileList): void {
+    this.loading.next(true);
+    // Convert FileList to an actual Array
+    const fileListArray: File[] = Array.from(fileList);
+
+    fileListArray.forEach((file: File) => {
+      this.reportFilesList.push(file);
+    });
+    this.loading.next(false);
+  }
+
+  public loadFilesForQuotation(fileList: FileList): void {
+    this.loading.next(true);
+    // Convert FileList to an actual Array
+    const fileListArray: File[] = Array.from(fileList);
+
+    fileListArray.forEach((file: File) => {
+      this.quotationFilesList.push(file);
+    });
+    this.loading.next(false);
   }
 }
 
