0d620c414415a8a514364c6be7850206b37bd353
Gustavo Alarcon <galarcon@meraki-s.com>
2022-03-15 11:57:02 -0500

    :rocket: deploy 7.24.27

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -22,7 +22,7 @@ export class AuthService {
   user$: Observable<User>;
   uiConfig: UiConfig;
   version$: Observable<GeneralConfig>;
-  version: string = 'V7.24.26r';
+  version: string = 'V7.24.27r';
 
   constructor(
     private afAuth: AngularFireAuth,
